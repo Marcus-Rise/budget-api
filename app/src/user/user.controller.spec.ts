1a7a46e463f23fb037ea485f995e7366a89fc493
Marcus-Rise <ilykonst95@gmail.com>
2022-05-17 21:47:54 +0400

    fix: tests

--- app/src/user/user.controller.spec.ts
+++ app/src/user/user.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -8,7 +10,13 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+        UserService,
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
