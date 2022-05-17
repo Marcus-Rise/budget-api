f6ddc662d8b4d0ff6f8f06cd340bfbaf647e956a
Marcus-Rise <ilykonst95@gmail.com>
2022-05-17 18:17:45 +0400

    feat: generate typeorm config

--- app/src/config/database.config.ts
+++ app/src/config/database.config.ts
@@ -1,20 +1,30 @@
 import { ConfigFactory } from '@nestjs/config/dist/interfaces/config-factory.interface';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { registerAs } from '@nestjs/config';
+import * as path from 'path';
 
 type DatabaseConfig = TypeOrmModuleOptions;
 
-const databaseConfigFactory: ConfigFactory<DatabaseConfig> = () => ({
-  type: 'postgres',
-  host: process.env.DATABASE_HOST ?? 'db',
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  entities: [__dirname + '../**/*.entity{.ts,.js}'],
-});
+const databaseConfigFactory: ConfigFactory<DatabaseConfig> = () => {
+  const baseUrl = path.resolve(__dirname, '..');
+  const migrationsDir: string = baseUrl + '/migrations';
+
+  return {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST ?? 'db',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [baseUrl + '/**/*.entity{.ts,.js}'],
+    migrations: [migrationsDir + '/*.{ts,js}'],
+    cli: {
+      migrationsDir,
+    },
+  };
+};
 
 const databaseConfig = registerAs('database', databaseConfigFactory);
 
-export { databaseConfig };
+export { databaseConfig, databaseConfigFactory };
 export type { DatabaseConfig };
