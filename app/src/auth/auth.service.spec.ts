deb00eeaaebd7918a827b4f6edb177f8d7587999
Marcus-Rise <ilykonst95@gmail.com>
2022-06-07 15:47:18 +0400

    test(auth): fix

--- app/src/auth/auth.service.spec.ts
+++ app/src/auth/auth.service.spec.ts
@@ -181,7 +181,7 @@ describe('AuthService', () => {
       expect(removeRefreshToken).toHaveBeenNthCalledWith(1, refreshToken);
     });
 
-    it('should throw error if token is revoked', async () => {
+    it('should throw error if user is not exists', async () => {
       verifyJwt.mockReturnValueOnce({ jti: 1, sub: 1 });
 
       const refreshToken = RefreshTokenEntityFactory.create(
