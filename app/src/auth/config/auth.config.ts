5f6756a3e606ea759312d7a265e72b6fd2686fa0
Marcus-Rise <ilykonst95@gmail.com>
2022-06-28 17:08:05 +0400

    refactor(auth): session ttl from ms

--- app/src/auth/config/auth.config.ts
+++ app/src/auth/config/auth.config.ts
@@ -1,20 +1,21 @@
 import { ConfigFactory } from '@nestjs/config/dist/interfaces/config-factory.interface';
 import { registerAs } from '@nestjs/config';
+import { StringValue } from 'ms';
+
+/** expressed in seconds or a string describing a time span [zeit/ms](https://github.com/zeit/ms.js).  Eg: 60, "2 days", "10h", "7d" */
+type SessionTTL = StringValue;
 
 const AUTH_CONFIG_TOKEN = 'auth';
 
-/**
- * @property {number} sessionTTL - seconds
- */
 type AuthConfig = {
-  sessionTTL: number;
+  sessionTTL: SessionTTL;
 };
 
 const authConfigFactory: ConfigFactory<AuthConfig> = () => ({
-  sessionTTL: parseInt(process.env.SESSION_TTL),
+  sessionTTL: process.env.SESSION_TTL as StringValue,
 });
 
 const authConfig = registerAs(AUTH_CONFIG_TOKEN, authConfigFactory);
 
 export { authConfig, AUTH_CONFIG_TOKEN };
-export type { AuthConfig };
+export type { AuthConfig, SessionTTL };
