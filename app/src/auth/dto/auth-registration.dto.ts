2962749e9bb56d508503f2c5e76d72a36f6b27bd
Ilya Konstantinov <ilykonst95@gmail.com>
2022-07-06 12:07:00 +0400

    fix(auth): login to lower case (#43)

--- app/src/auth/dto/auth-registration.dto.ts
+++ app/src/auth/dto/auth-registration.dto.ts
@@ -1,8 +1,11 @@
-import { IsEmail, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 import { PASSWORD_MIN_LENGTH } from './validators';
+import { Transform } from 'class-transformer';
 
 class AuthRegistrationDto {
+  @IsNotEmpty()
   @IsEmail()
+  @Transform(({ value }) => value.toLowerCase(), { toClassOnly: true })
   login: string;
 
   @MinLength(PASSWORD_MIN_LENGTH)
